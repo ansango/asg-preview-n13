27181a4364080a7a2549eaf043a3936adae6fb86
ansango <anibalsantosgo@gmail.com>
2023-03-17 18:46:17 +0100

    fix(params in basic): basic params routes

--- src/app/[filename]/page.tsx
+++ src/app/[filename]/page.tsx
@@ -1,24 +1,11 @@
 import type { Metadata } from "next";
 
 import { Blocks } from "@/components/cms";
-import { getPage, getPageConnection, getSeries } from "@/lib";
+import { getPage, getSeries } from "@/lib";
 
 type Params = {
   filename: string;
 };
-
-export async function generateStaticParams() {
-  const pages = await getPageConnection();
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
-  return pages.map((page) => ({
-    params: {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      filename: page._sys.filename,
-    },
-  }));
-}
 
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
   const data = await getPage({ params });
@@ -28,7 +15,6 @@ export async function generateMetadata({ params }: { params: Params }): Promise<
 }
 
 export default async function Page({ params }: { params: Params }) {
-  console.log(params);
   const series = params.filename === "series" ? await getSeries() : null;
   const data = await getPage({ params });
 
